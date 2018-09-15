d593ae382256c47c3a626a0d3715b73695080eba
Eric Olson <eric.rob.olson@gmail.com>
2018-09-15 09:29:19 -0700

    Added GraphicsSystem.

---
+++ include/Systems/GraphicsSystem.h
@@ -0,0 +1,18 @@
+#ifndef GRAPHICSSYSTEM_H
+#define GRAPHICSSYSTEM_H
+
+#include "BaseSystem.h"
+
+
+class GraphicsSystem : public BaseSystem
+{
+    public:
+        GraphicsSystem();
+        virtual ~GraphicsSystem();
+
+    protected:
+
+    private:
+};
+
+#endif // GRAPHICSSYSTEM_H
---
+++ src/Systems/GraphicsSystem.cpp
@@ -0,0 +1,11 @@
+#include "GraphicsSystem.h"
+
+GraphicsSystem::GraphicsSystem() : BaseSystem()
+{
+    //ctor
+}
+
+GraphicsSystem::~GraphicsSystem()
+{
+    //dtor
+}
