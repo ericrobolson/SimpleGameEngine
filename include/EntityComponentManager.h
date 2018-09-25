e915ece6ecd388d31c8ea63330542cc78458cdc8
Eric Olson <eric.rob.olson@gmail.com>
2018-09-25 12:57:14 -0700

    Started implementing EntityComponentManager. Still need to ensure that pointers and references are done correctly as well as implement other functionality.

--- include/EntityComponentManager.h
+++ include/EntityComponentManager.h
@@ -3,36 +3,47 @@
 #include <typeindex>
 #include <typeinfo>
 #include <unordered_map>
+#include <list>
 #include "BaseComponent.h"
 
 
 // !!! Todo: Refactor the componentsTable into an array, where the type is hashed. Should be more memory effecient when performing lookups and allows static allocation on things
 // use this solution: https://stackoverflow.com/a/9859605
 
-typedef std::unordered_map<std::type_index, BaseComponent[]> TypeMap;
+typedef std::unordered_map<std::type_index, BaseComponent*> TypeMap;
 
 class EntityComponentManager
 {
     public:
-        const int MAXNUMBEROFENTITIES = 1'000'000; // Should be smaller than 2147483647, the maximum int size as that's what's used to declare the size of arrays of components
-        const int MAXNUMBEROFCOMPONENTTABLES = 200;
+        static const int MAXNUMBEROFENTITIES = 100'000; // Should be smaller than 2147483647, the maximum int size as that's what's used to declare the size of arrays of components
+        static const int MAXNUMBEROFCOMPONENTTABLES = 200;
         EntityComponentManager();
         virtual ~EntityComponentManager();
 
-        int? AddEntity(){
-            // get first available id from id pool
-        }; // Returns the id of the added entity; if no entity was added, return null. This may be the case if there are no available free ids
+        int AddEntity(); // Returns the id of the added entity, which is the first free identity; if no entity was added, return -1. This may be the case if there are no available free ids.
+
+        void MarkEntityInactive(int entityId); // marks the given entity as inactive and adds it to the list of inactiveEntityIds
+
+        int GetEntitiesCount(); // get an count of all active entities
+
+        // deletes all inactive entities and components, and then adds those deleted ids to the free id pool; should always be called at the end of the game loop, during the cleanup phase
+        // clears the inactiveEntityIds and takenEntityIds after processing and adds them back to the availableEntityIds
+        void DeleteAllInactiveEntities();
+
 
         // Add that component to the entity.
         // See if that 'table', or the place in memory for that component type exists.
         // If 'table' does exist, check to see if GetComponent() exists. if so, then return that item.
         // If 'table' does not exist, then initialize a new fixed array of size MAXNUMBEROFENTITIES, for that component type, setting each item to null.
         // Basically, an entity is an index, or arrayComponents[entityId]. If arrayComponents[entityId] is null, that means there's no component for that entity.
-        // Now, add the component to the index of 'entityId'
+        // Now, add the component to the index of 'entityId'takenEntityIds
         // Return the component
         template <class TComponent>
         TComponent& AddComponent(int entityId){
-            TComponent[]  componentTable = _componentTables[typeid(TComponent)];
+
+            ///todo: Ensure that references/pointers are used properly
+
+            TComponent* componentTable = _componentTables[typeid(TComponent)];
 
             if (componentTable == nullptr){
                 if (_componentTypesAdded >= MAXNUMBEROFCOMPONENTTABLES){
@@ -60,17 +71,15 @@ class EntityComponentManager
             return componentTable[entityId];
         };
 
+        /*
+
         template <class TComponent>
         TComponent& GetComponent(int entityId) // gets the type of component for that entity, returns null if that component was not found
-
-        void MarkEntityInactive(int entityId); // marks the given entity as inactive and adds it to the list of inactiveEntityIds
-
-        int GetEntitiesCount(); // get an count of all active entities
 
         void AddSearch(TComponent) // adds to the end of _typesOfComponentsToSearch the type of component to search on
         list int ExecuteSearch() // returns a list of entity ids with the given search; starts off by calling ExecuteSearch(list int idsToSearch) on all entity ids
         {
-            list int matchingIds = ExecuteSearch(allIds);
+            list int matchingIds = ExecuteSearch(takenEntityIds);
 
             // Always reset the search, in case the list of matching entities was empty and there are still search terms left. This allows it to always start fresh so other classes don't have to call reset.
             _typesOfComponentsToSearch.clear();
@@ -78,30 +87,29 @@ class EntityComponentManager
 
             return matchingIds;
         }
-
-
-        // deletes all inactive entities and components, and then adds those deleted ids to the free id pool; should always be called at the end of the game loop, during the cleanup phase
-        // clears the inactiveEntityIds and takenEntityIds after processing and adds them back to the availableEntityIds
-        void DeleteInactiveEntites();
-
+        */
 
     protected:
 
     private:
+        std::list<int> _inactiveEntityIds; // a list of all entity ids to cleanup
+        std::list<int> _availableEntityIds; // a list of all available entity ids
+        std::list<int>  _takenEntityIds; // a list of entity ids that are taken
+        int _componentTypesAdded;
         TypeMap _componentTables;
-        list <BaseComponent[], componenttypename> _componentTables; // a list of tuples, one containing the typeid(componentTye).name() string and one containing an array of those pointers
-        list TypesOfComponentsToSearch _typesOfComponentsToSearch; // the list of types of components to search for; implemented as a queue
-        int _componentTypesAdded = 0;
-        list int inactiveEntityIds; // a list of all entity ids to cleanup
 
-        list int availableEntityIds; // a list of all available entity ids
-        list int takenEntityIds; // a list of entity ids that are taken
+
+        /*
+        list TypesOfComponentsToSearch _typesOfComponentsToSearch; // the list of types of components to search for; implemented as a queue
+
 
         // given the list of ids to search, dequeue the the next _typesOfComponentsToSearch item,
         // then return a subset of idsToSearch that meet the _typesOfComponentsToSearch item.
         // Use the GetComponent(entityId, TComponentTYpe) to see if the entity has that component
         // If _typepsOfComponentsToSearch is empty, reeturn the entity ids. If the idsToSearch is empty, then return empty list
         list int ExecuteSearch(list int idsToSearch)
+
+        */
 };
 
 #endif // ENTITYCOMPONENTMANAGER_H
--- src/EntityComponentManager.cpp
+++ src/EntityComponentManager.cpp
@@ -1,24 +1,59 @@
+#include <list>
 #include "EntityComponentManager.h"
 
 EntityComponentManager::EntityComponentManager()
 {
-    //ctor
+    // Initialize the list of free entity ids
+    for (int i = 0; i < EntityComponentManager::MAXNUMBEROFENTITIES; i++){
+        _availableEntityIds.push_back(i);
+    }
+
+    _componentTypesAdded = 0;
 }
 
 EntityComponentManager::~EntityComponentManager()
 {
     // mark all entities as inactive, so that they are marked for cleanup
+    while(_takenEntityIds.empty() == false){
+        int entityId = _takenEntityIds.front();
 
-    for (int i = 0; i < takenEntityIds.count(); i++){
+        _takenEntityIds.pop_front();
 
-        int entityToMarkAsInactive = takenEntityIds[i];
-
-        MarkEntityInactive(entityToMarkAsInactive);
+        MarkEntityInactive(entityId);
     }
 
-    DeleteInactiveEntites();
+    DeleteAllInactiveEntities();
 
     // delete other misc memory shit
 
     //dtor
+
+}
+
+void EntityComponentManager::DeleteAllInactiveEntities(){
+    // todo: remove inactive entities and clean up components
+}
+
+/// Get a count of active entities
+int EntityComponentManager::GetEntitiesCount(){
+    return _takenEntityIds.size();
+}
+
+/// Mark the given entity as inactive.
+void EntityComponentManager::MarkEntityInactive(int entityId){
+    _inactiveEntityIds.push_back(entityId);
+}
+
+/// Return the id of the added entity; if -1, an entity was not created. 0 is a valid id.
+int EntityComponentManager::AddEntity(){
+    int entityId = -1;
+
+    if (_availableEntityIds.empty() == false){
+        entityId = _availableEntityIds.front();
+
+        _availableEntityIds.pop_front();
+        _takenEntityIds.push_back(entityId);
+    }
+
+    return entityId;
 }
