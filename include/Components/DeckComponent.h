5442e128c338217df2223dcdadc248e9de70bc9b
Eric Olson <eric.rob.olson@gmail.com>
2018-11-14 10:07:31 -0800

    Added config. Started fleshing out cards.

--- include/Components/DeckComponent.h
+++ include/Components/DeckComponent.h
@@ -2,7 +2,116 @@
 #define DECKCOMPONENT_H
 
 #include "BaseComponent.h"
+#include <vector>
+#include <memory>
 
+#include "EntityComponentManager.h"
+
+/*
+Example Cards:
+    Name: Melee
+    Cost: 1
+    CardType: Attack
+    PowerType: OneShot
+    Effect: Create an Attack with 5 damage, discard after use
+
+    Name: Shield
+    CardType: Shield
+    Cost: 1
+    PowerType: OneShot
+    Effect: Create a shield with 10hp, discard after use
+
+    Name: Next Turn
+    Cost: 0
+    CardType: Spell
+    PowerType: OneShot
+    Effect: Discard hand, start next turn, exile this card, add a copy of this card to your hand
+
+    Name: Energy Reserves
+    Cost: 2
+    CardType: Spell
+    PowerType: Modifier
+    Effect: At the start of your turn, gain 1 energy
+
+    Name: Fire
+    Cost: 1
+    CardType: Spell
+    PowerType: Trigger
+    Effect: Your next attack gains fire, discard this card
+
+    Name: Ice
+    Cost: 1
+    CardType: Spell
+    PowerType: Trigger
+    Effect: Your next attack gains Ice, discard this card
+
+    Name: Golden Coin
+    Cost: 2
+    CardType: Spell
+    PowerType: OneShot
+    Effect: Gain 3 energy, discard this card
+
+    Name: Critical Strike
+    Cost: 1
+    CardType: Spell
+    PowerType: Trigger
+    Effect: Your next attack deals 2x damage, exile that card, discard this card
+
+    Name: Critical Shield
+    Cost: 1
+    CardType: Spell
+    PowerType: Trigger
+    Effect: Your next Shield gains 2x shield, exile that card, discard this card
+
+    Name: Health Pot
+    Cost: 1
+    CardType: Spell
+    PowerType: OneShot
+    Effect: You gain 10hp, exile this card
+
+    Name: Consuming Hunger
+    Cost: 0
+    CardType: Spell
+    PowerType: OneShot
+    Effect: Exile top card of deck, gain energy equal to it's cost, discard this card
+
+    Name: Shiv
+    Cost: 0
+    CardType: Attack
+    PowerType: OneShot
+    Effect: Create an attack with 4 damage, exile this card
+
+    Name: Blade Master
+    Cost: 1
+    CardType: Spell
+    PowerType: Trigger
+    Effect: At the start of your turn, add 1 shiv to your hand
+
+    Name: Strongman
+    Cost: 3
+    CardType: Spell
+    PowerType: Trigger
+    Effect: At the start of your turn, gain +1 Strength
+
+    Name: ???
+    Cost: 1
+    CardType: Attack
+    PowerType: OneShot
+    Effect: Draw 1 card, create an attack with 9 dmg
+*/
+
+
+
+class Card{
+    public:
+        int EnergyCost;
+
+        enum CardTypes {Attack, Shield, Spell};
+        CardTypes CardType;
+
+        enum PowerTypes {OneShot, Modifier, Trigger};
+        PowerTypes PowerType;
+};
 
 class DeckComponent : public BaseComponent
 {
@@ -10,20 +119,34 @@ class DeckComponent : public BaseComponent
         DeckComponent();
         virtual ~DeckComponent();
 
-        void NextCard();
-        void PreviousCard();
-
-        void PeekPreviousCard();
-        void PeekNextCard();
-        void PeekActiveCard();
+        void SelectNextCard();
+        void SelectPreviousCard();
 
         void UseActiveCard();
-        void ResetDeck();
+        void ShuffleDeck();
+
+        void BeginTurn();
+        void EndTurn();
+
+        void DrawCards(int numCards);
+
+        int GetEnergy();
+        int AddEnergy(int energyToAdd);
+
+
+        void ProcessTriggers(ECS::EntityComponentManager entityComponentManager, int entityIdForCard);
 
 
     private:
-        int _index;
-        int _deckSize;
+        const int DefaultEnergy = 3;
+
+        int _energy;
+        int _activeCardIndex;
+
+        std::vector<Card> _hand;
+        std::vector<Card> _deck;
+        std::vector<Card> _exhaustedCards;
+        std::vector<Card> _discardedCards;
 };
 
 #endif // DECKCOMPONENT_H
