2dcb10cfd7b6c0bd07dba2ad150d900a6dc3647f
Eric Olson <eric.rob.olson@gmail.com>
2019-01-23 07:58:28 -0800

    Continued adding ECS tests.

--- tests/EntityComponentManager/EntityComponentManagerTests.cpp
+++ tests/EntityComponentManager/EntityComponentManagerTests.cpp
@@ -16,10 +16,18 @@ public:
     int TestNumber;
 };
 
+class TestComponent2 : public BaseComponent{
+public:
+    const bool TestFlagDefaultValue = true;
+    const int TestNumberDefaultValue = 1;
+
+    virtual ~TestComponent2(){};
+    TestComponent2(): BaseComponent(){ TestFlag = TestFlagDefaultValue; TestNumber = TestNumberDefaultValue;};
+    bool TestFlag;
+    int TestNumber;
+};
+
 //tests todo:
-    //             std::vector<int> Search(std::function<bool(TComponent c)> const& filterLambda)
-    //             std::vector<int> Search()
-    //             std::vector<int> SearchOn(std::vector<int> entityIds)
     //             std::vector<int> SearchOn(std::vector<int> entityIds, std::function<bool(TComponent c)> const& filterLambda){
     //             void RemoveComponent(int entityId)
     //             void MarkEntityInactive(int entityId)
@@ -27,10 +35,86 @@ public:
     //             void Reset()
 
 
-//tests in progress:
+//             std::vector<int> Search(std::function<bool(TComponent c)> const& filterLambda)
 
-// std::shared_ptr<TComponent> GetComponent(int entityId)
 
+SCENARIO("EntityComponentManager::SearchOn_MatchingEntityIds_ReturnsExpectedResults"){
+    EntityComponentManager ecs;
+
+    std::shared_ptr<int> entityPtr1 = ecs.AddEntity();
+    int entityId1 = *entityPtr1.get();
+
+    ecs.AddComponent<TestComponent>(entityId1);
+
+    std::shared_ptr<int> entityPtr2 = ecs.AddEntity();
+    int entityId2 = *entityPtr2.get();
+
+    ecs.AddComponent<TestComponent>(entityId2);
+    ecs.AddComponent<TestComponent2>(entityId2);
+
+    std::vector<int> entitiesToSearchOn = ecs.Search<TestComponent>();
+
+    std::vector<int> matchingEntityIds = ecs.SearchOn<TestComponent2>(entitiesToSearchOn);
+
+    REQUIRE(matchingEntityIds.empty() == false);
+    REQUIRE(matchingEntityIds.size() == 1);
+    REQUIRE(matchingEntityIds[0] == entityId2);
+}
+
+SCENARIO("EntityComponentManager::SearchOn_EmptyEntityIds_ReturnsEmptyVector"){
+    EntityComponentManager ecs;
+
+    std::vector<int> entitiesToSearchOn;;
+
+    std::vector<int> matchingEntityIds = ecs.SearchOn<TestComponent>(entitiesToSearchOn);
+
+    REQUIRE(matchingEntityIds.empty() == true);
+}
+
+SCENARIO("EntityComponentManager::Search_MatchingEntities_ReturnsVectorWithMatchingIds"){
+    EntityComponentManager ecs;
+
+    std::shared_ptr<int> entityPtr = ecs.AddEntity();
+    int entityId = *entityPtr.get();
+
+    ecs.AddComponent<TestComponent>(entityId);
+
+    ecs.AddEntity();
+
+    std::vector<int> matchingEntityIds = ecs.Search<TestComponent>();
+
+    REQUIRE(matchingEntityIds.empty() == false);
+    REQUIRE(matchingEntityIds.size() == 1);
+    REQUIRE(matchingEntityIds[0] == entityId);
+}
+
+SCENARIO("EntityComponentManager::Search_NoMatchingEntities_ReturnsEmptyVector"){
+    EntityComponentManager ecs;
+
+    std::vector<int> matchingEntityIds = ecs.Search<TestComponent>();
+
+    REQUIRE(matchingEntityIds.empty() == true);
+}
+
+SCENARIO("EntityComponentManager::GetComponent_NonExistingComponent_ReturnsNull"){
+    EntityComponentManager ecs;
+    std::shared_ptr<int> entityPtr = ecs.AddEntity();
+    int entityId = *entityPtr.get();
+
+    std::shared_ptr<TestComponent> testComponentPtr = ecs.GetComponent<TestComponent>(entityId);
+    REQUIRE(testComponentPtr == nullptr);
+}
+
+SCENARIO("EntityComponentManager::GetComponent_ExistingComponent_ReturnsExistingComponentForEntity"){
+    EntityComponentManager ecs;
+    std::shared_ptr<int> entityPtr = ecs.AddEntity();
+    int entityId = *entityPtr.get();
+
+    ecs.AddComponent<TestComponent>(entityId);
+
+    std::shared_ptr<TestComponent> testComponentPtr = ecs.GetComponent<TestComponent>(entityId);
+    REQUIRE(testComponentPtr != nullptr);
+}
 
 SCENARIO("EntityComponentManager::AddComponent_ExistingComponent_ReturnsExistingComponentForEntity"){
     EntityComponentManager ecs;
@@ -84,7 +168,3 @@ SCENARIO("EntityComponentManager::AddEntity_AddEntitiesPastMaxEntitities_Returns
 
     REQUIRE(actualId == nullptr);
 }
-
-
-
-
