0e046e5a98235ecaecab14fc03c88d0b91b8f21d
Eric Olson <eric.rob.olson@gmail.com>
2019-05-09 22:48:13 -0700

    Continued work on OpenGL.

--- include/Systems/GraphicsSystem_3d.h
+++ include/Systems/GraphicsSystem_3d.h
@@ -4,7 +4,11 @@
 
 #include "EntityComponentManager.h"
 #include "BaseSystem.h"
+#define GL_GLEXT_PROTOTYPES
 
+
+#include <SDL.h>
+#include <SDL_opengl.h>
 
 class GraphicsSystem_3d
 {
@@ -12,7 +16,12 @@ class GraphicsSystem_3d
         GraphicsSystem_3d();
         virtual ~GraphicsSystem_3d();
         bool Process(ECS::EntityComponentManager &ecs);
+    private:
+        void DrawScrene();
 
+        SDL_Window* _sdlWindow;
+        SDL_GLContext* _glContext;
+        GLuint _vao, _vbo;
 
 };
 
